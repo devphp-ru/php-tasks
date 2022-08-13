ba07f4da0228744d3bb1741d5457315917c72687
WebHumpty <webxak31@yandex.ru>
2022-08-13 15:37:15 +0300

    Задачи на date

--- src/tasks/date.php
+++ src/tasks/date.php
@@ -13,3 +13,23 @@ foreach ($period as $value) {
     $dates[] = $value->format('d.m.Y');
 }
 print_r($dates);
+
+/**
+ * Формирование массива, содержащий все дни из интервала на основе strtotime
+ */
+function getDates(string $start, string $end, $format = 'd.m.Y'): array
+{
+    $day = 86400;
+    $start = strtotime($start . '-1 days');
+    $end = strtotime($end . '+1 days');
+    $n = round(($end - $start) / $day);
+
+    $dates = [];
+    for ($i = 1; $i < $n; $i++) {
+        $dates[] = date($format, ($start + ($i * $day)));
+    }
+
+    return $dates;
+}
+
+print_r(getDates('01.08.2022', '13.08.2022'));
