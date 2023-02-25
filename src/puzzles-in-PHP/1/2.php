20c1bc64f59cfd73507808097b2cbda70a81569c
devphpru <answebdev@yandex.ru>
2023-02-25 21:31:40 +0300

    Регулярные выражения

--- src/puzzles-in-PHP/1/2.php
+++ src/puzzles-in-PHP/1/2.php
@@ -33,3 +33,31 @@ $pattern = "#[ \f\t\v]+#s";
 $replace = ' ';
 $text = preg_replace($pattern, $replace, $text);
 echo $text;
+
+/**
+ * Извлечение названия HTML-страницы
+ */
+$content = file_get_contents(__DIR__ . '/file.php');
+$pattern = '#<title>(.*)</title>#siU';
+if (preg_match($pattern, $content, $matches)) {
+	echo $matches[1];
+}
+
+/**
+ * Преобразование даты из формата YYYY-MM-DD в DD.MM.YYYY
+ */
+$date = '2023-02-25';
+$pattern = '#([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})#';
+preg_match($pattern, $date, $matches);
+echo "{$matches[3]}.{$matches[2]}.{$matches[1]}";//25.02.2023
+
+/**
+ * Проверка корректности адреса электронной почты
+ */
+$email = 'support@example.com';
+$pattern = '#^[-0-9a-z_]+@[-0-9a-z^\.]+\.[a-z]{2,6}$#i';
+if (!preg_match($pattern, $email)) {
+	echo 'Вы ввели некорректный адрес электронной почты.';
+} else {
+	echo $email;
+}
