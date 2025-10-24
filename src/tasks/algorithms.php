5a226666e995d2766ee65a158fd575d65b2b0192
devphpru <wixphp@gmail.com>
2025-10-24 21:07:27 +0300

    Алгоритмы на PHP

---
+++ src/tasks/algorithms.php
@@ -0,0 +1,174 @@
+<?php
+
+/**
+ * Сортировка пузырьком (Bubble Sort) — простой алгоритм сортировки, основанный на многократном проходе по сортируемому массиву.
+ * Классическая формулировка: дан массив из n элементов, требуется упорядочить его по возрастанию (или убыванию) значений элементов.
+ * Временная сложность алгоритма — O(n²) в худшем и среднем случаях, где n — количество элементов.
+ */
+function bubbleSort(array $arr): array
+{
+    $n = count($arr);
+
+    if ($n <= 1) {
+        return $arr;
+    }
+
+    for ($i = 0; $i < $n; $i++) {
+        for ($j = ($n - 1); $j > $i; $j--) {
+            if ($arr[$j] < $arr[$j - 1]) {
+                $tmp = $arr[$j];
+                $arr[$j] = $arr[$j - 1];
+                $arr[$j - 1] = $tmp;
+            }
+        }
+    }
+
+    return $arr;
+}
+$arr = [5, 2, 1, 3, 4];
+$result = bubbleSort($arr);
+print_r($result);
+
+/**
+ * Сортировка вставками (Insertion sort) — алгоритм сортировки, в котором элементы входной
+ * последовательности просматриваются по одному, и каждый новый поступивший элемент размещается
+ * в подходящее место среди ранее упорядоченных элементов.
+ * Временная сложность алгоритма — O(n²) в среднем и в худшем случаях. В лучшем случае (когда массив уже отсортирован) сложность — O(n).
+ */
+function insertSort(array $arr): array
+{
+    $n = count($arr);
+
+    if ($n <= 1) {
+        return $arr;
+    }
+
+    for ($i = 1; $i < $n; $i++) {
+        $value = $arr[$i];
+        $j = ($i - 1);
+
+        while (isset($arr[$j]) && ($arr[$j] > $value)) {
+            $arr[$j + 1] = $arr[$j];
+            $arr[$j] = $value;
+            $j--;
+        }
+    }
+
+    return $arr;
+}
+$arr = [5, 2, 1, 3, 4];
+$result = insertSort($arr);
+print_r($result);
+
+/**
+ * Сортировка выбором (Selection Sort) — алгоритм сортировки, который находит наименьший
+ * (или наибольший) элемент в массиве и перемещает его в начало (или конец).
+ * Временная сложность алгоритма — в худшем случае O(n²), где n — количество элементов в массиве.
+ */
+function selectSort(array $arr): array
+{
+    $n = count($arr);
+
+    if ($n <= 1) {
+        return $arr;
+    }
+
+    for ($i = 0; $i < $n; $i++) {
+        $k = $i;
+        for ($j = ($i + 1); $j < $n; $j++) {
+            if ($arr[$k] > $arr[$j]) {
+                $k = $j;
+            }
+        }
+
+        if ($k != $i) {
+            $tmp = $arr[$i];
+            $arr[$i] = $arr[$k];
+            $arr[$k] = $tmp;
+        }
+    }
+
+    return $arr;
+}
+$arr = [5, 2, 1, 3, 4];
+$result = selectSort($arr);
+print_r($result);
+
+/**
+ * Сортировка слиянием (англ. Merge sort) — алгоритм сортировки, основанный на принципе «разделяй и властвуй».
+ * Временная сложность алгоритма — O(n log n), где n — количество элементов в массиве.
+ * Это связано с тем, что массив делится на две части логарифмическое количество раз (log n),
+ * и каждая операция слияния требует линейного времени (O(n)).
+ */
+function mergeSort(array $arr): array
+{
+    $n = count($arr);
+
+    if ($n <= 1) {
+        return $arr;
+    }
+
+    $left = array_slice($arr, 0, (int)($n / 2));
+    $right = array_slice($arr, (int)($n / 2));
+
+    $left = mergeSort($left);
+    $right = mergeSort($right);
+
+    return merge($left, $right);
+}
+function merge(array $left, array $right): array
+{
+    $result = [];
+
+    while (count($left) > 0 && count($right) > 0) {
+        if ($left[0] < $right[0]) {
+            $result[] = array_shift($left);
+        } else {
+            $result[] = array_shift($right);
+        }
+    }
+
+    array_splice($result, count($result), 0, $left);
+    array_splice($result, count($result), 0, $right);
+
+    return $result;
+}
+$arr = [5, 2, 1, 3, 4];
+$result = mergeSort($arr);
+print_r($result);
+
+/**
+ * Быстрая сортировка (Quick Sort) — алгоритм сортировки, основанный на принципе «разделяй и властвуй».
+ * Разработан в 1960 году Чарльзом Энтони Ричардом Хоаром .
+ * Временная сложность алгоритма:
+ * В среднем случае — O(n log n).
+ * В худшем случае — O(n²).
+ */
+function quickSort(array $arr): array
+{
+    $n = count($arr);
+
+    if ($n <= 1) {
+        return $arr;
+    }
+
+    $firstValue = $arr[0];
+    $left = [];
+    $right = [];
+
+    for ($i = 1; $i < $n; $i++) {
+        if ($arr[$i] <= $firstValue) {
+            $left[] = $arr[$i];
+        } else {
+            $right[] = $arr[$i];
+        }
+    }
+
+    $left = quickSort($left);
+    $right = quickSort($right);
+
+    return array_merge($left, [$firstValue], $right);
+}
+$arr = [5, 2, 1, 3, 4];
+$result = quickSort($arr);
+print_r($result);
