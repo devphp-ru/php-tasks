ed663ac68d248341af1822c6c8775846de69bede
devphpru <wixphp@gmail.com>
2026-04-16 15:06:04 +0300

    Запросы SQL (подзапросы)

--- src/sql/subqueries.sql
+++ src/sql/subqueries.sql
@@ -55,3 +55,36 @@ select job_id, first_name, last_name, salary from employees where salary > all (
 
 select e1.first_name, e1.last_name from employees e1 where not exists (select 'x' from employees e2 where e2.manager_id=e1.employee_id);
 
+-- 13. Напишите запрос, чтобы отобразить идентификатор сотрудника, имя, фамилию и названия отделов всех сотрудников.
+
+select employee_id, first_name, last_name, (select department_name from departments d where e.department_id=d.department_id) departemnt from employees e order by departemnt asc;
+
+-- 14. Напишите запрос, чтобы найти пятую максимальную зарплату в таблице сотрудников.
+
+select distinct salary from employees e1 where 5 = (select count(distinct salary) from employees e2 where e2.salary>=e1.salary);
+
+-- 15. Напишите запрос, чтобы найти четвертую минимальную зарплату в таблице сотрудников.
+
+select distinct salary from employees e1 where 4 = (select count(distinct salary) from employees e2 where e2.salary <= e1.salary);
+
+-- 16. Напишите запрос, чтобы выбрать последние 10 записей из таблицы.
+
+select * from (select * from employees order by employee_id desc limit 10) sub order by employee_id asc;
+
+select * from employees order by employee_id desc limit 10;
+
+-- 17. Напишите запрос с указанием идентификатора и названия всех отделов, в которых не работает ни один сотрудник.
+
+select * from departments where department_id not in (select department_id from employees);
+
+-- 18. Напишите запрос, чтобы получить 3 максимальные зарплаты.
+
+select distinct salary from employees e1 where 3 >= (select count(distinct salary) from employees e2 where e2.salary >= e1.salary) order by e1.salary desc;
+
+-- 19. Напишите запрос, чтобы получить 3 минимальные зарплаты.
+
+select distinct salary from employees e1 where 3 >= (select count(distinct salary) from employees e2 where e2.salary <= e1.salary) order by e1.salary desc;
+
+-- 20. Напишите запрос, чтобы получить максимальную зарплату сотрудников.
+
+select * from employees e1 where (1) = (select count(distinct(e2.salary)) from employees e2 where e2.salary > e1.salary);
